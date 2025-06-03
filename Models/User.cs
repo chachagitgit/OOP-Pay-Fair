1bc1a3edf4b79b940ae660afae3db9814762ca76
Jerleeace <jerleesoncedriquegalipio@iskolarngbayan.pup.edu.ph>
2025-06-04 01:10:31 +0800

    homepage working but no background image

--- Models/User.cs
+++ Models/User.cs
@@ -3,8 +3,8 @@ namespace OOP_Fair_Fare.Models
     public class User
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
+        public required string UserName { get; set; }
+        public required string Email { get; set; }
         // Add other properties as needed
     }
 }
--- obj/Debug/net9.0/OOP-Fair-Fare.AssemblyInfo.cs
+++ obj/Debug/net9.0/OOP-Fair-Fare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP-Fair-Fare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65cdfb722845fef92fdabe047f2ac99ae5ff80c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c7dd9a2755654bc739ef1c287dbca2c9c3fdb92")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP-Fair-Fare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP-Fair-Fare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
