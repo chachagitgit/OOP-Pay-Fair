d268ff9aa746c4c801425db457a1b45574804c69
Jerleeace <jerleesoncedriquegalipio@iskolarngbayan.pup.edu.ph>
2025-06-10 20:12:35 +0800

    github ask to commit cuz i cant pull

--- Models/User.cs
+++ Models/User.cs
@@ -5,6 +5,7 @@ namespace OOP_Fair_Fare.Models
         public int Id { get; set; }
         public required string UserName { get; set; }
         public required string Email { get; set; }
+        public required string Password { get; set; }
         // Add other properties as needed
     }
 }
