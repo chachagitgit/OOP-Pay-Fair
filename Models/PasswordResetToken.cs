b602ae7d08ed525aec51cd7b53c2c3b598933a78
chachagitgit <cha.caramoan@gmail.com>
2025-06-24 22:25:10 +0800

    syntax fixed

--- Models/PasswordResetToken.cs
+++ Models/PasswordResetToken.cs
@@ -1,7 +1,9 @@
 namespace OOP_Fair_Fare.Models
 {
+    //encapsulation
     public class PasswordResetToken
-    {        public int Id { get; set; }
+    {
+        public int Id { get; set; }
         public int UserId { get; set; }
         public required string Token { get; set; }
         public DateTime ExpiryDate { get; set; }
